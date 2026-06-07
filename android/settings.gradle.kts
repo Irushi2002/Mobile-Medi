03dcf5017ed75a08f61270a1773833a2cf18a543
Irushi2002 <iruhewage@gmail.com>
2026-06-07 09:48:35 +0530

    fix google authentication

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,4 +1,4 @@
-pluginManagement {
+﻿pluginManagement {
     val flutterSdkPath = run {
         val properties = java.util.Properties()
         file("local.properties").inputStream().use { properties.load(it) }
@@ -6,21 +6,17 @@ pluginManagement {
         require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
         flutterSdkPath
     }
-
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
-
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
-
 include(":app")
